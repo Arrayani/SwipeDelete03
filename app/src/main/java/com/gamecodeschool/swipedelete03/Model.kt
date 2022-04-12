2991b9675f0b8cff74c4883a71d6ba414a1c8704
Roni <rampage9198@yahoo.com>
2022-04-12 16:26:18 +0700

    Initial commit

---
+++ app/src/main/java/com/gamecodeschool/swipedelete03/Model.kt
@@ -0,0 +1,15 @@
+package com.gamecodeschool.swipedelete03
+
+/**
+
+ * Author: Roni Reynal Fitri  on $ {DATE} $ {HOUR}: $ {MINUTE}
+
+ * Email: biruprinting@gmail.com
+
+ */
+class Model {
+
+    var name: String? = null
+
+
+}
